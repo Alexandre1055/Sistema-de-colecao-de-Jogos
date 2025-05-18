5d4c3774bc76da4034e2ead3d2516066ed4f2842
Alexandre <alexandrebatisata.123@hotmail.com>
2025-05-18 15:45:29 -0300

    Game seed

--- dslist/src/main/java/com/alexandre/dslist/entities/Game.java
+++ dslist/src/main/java/com/alexandre/dslist/entities/Game.java
@@ -23,14 +23,18 @@ public class Game {
 	private String genre;
 	private String platforms;
 	private Double score;
-	private String imgUri;
+	private String imgUrl;
+
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUri,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
@@ -38,7 +42,7 @@ public class Game {
 		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
-		this.imgUri = imgUri;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -91,12 +95,12 @@ public class Game {
 		this.score = score;
 	}
 
-	public String getImgUri() {
-		return imgUri;
+	public String getImgUrl() {
+		return imgUrl;
 	}
 
-	public void setImgUri(String imgUri) {
-		this.imgUri = imgUri;
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
